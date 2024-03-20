045eb9a47b90001e3531d2a6fbd191bafa74b448
JerryLin <iii20220906linjerry@gmail.com>
2024-03-20 21:11:53 +0800

    feat: build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -46,6 +47,7 @@ export default defineConfig({
         chunkFileNames: 'assets/[hash].js',
         assetFileNames: 'assets/[hash].[ext]'
       }
-    }
+    },
+    index: path.resolve(__dirname, '../dist/index.html')
   }
 });
