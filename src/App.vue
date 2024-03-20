a88120ec6c4fb4cd66953bb75bb24db4a840ef83
JerryLin <iii20220906linjerry@gmail.com>
2024-03-20 21:21:13 +0800

    feat:build

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,18 @@ import Header from '@/components/Header.vue';
 import { RouterView, RouterLink } from 'vue-router';
 import '@/style/dashboard.scss'
 import { useWeatherStore } from '@/stores/weather';
-const { weatherData, weatherInfo,getWeather } = useWeatherStore();
+const { weatherData, weatherInfo, getWeather } = useWeatherStore();
 
 onMounted(() => {
-    // console.log(123);
-    getWeather();
-  });
+  // console.log(123);
+  getWeather();
+  setTimeout(() => {
+
+    router.push({
+      name: "list", // 路由的名稱
+    });
+  }, 1000)
+});
 </script>
 
 <style scoped>
